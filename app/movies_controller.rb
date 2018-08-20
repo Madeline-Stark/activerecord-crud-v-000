39b54e365420b29e8e78dafe8edbefeab606fc35
Madeline Stark <madeline.stark1@gmail.com>
2018-08-20 16:52:12 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,7 +29,9 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   # release_date == 1990
 
   Movie.create do |m|
-    __
+    m.each {|key, value| 
+      self.send(("#{key}="), value)}
+    end
   end
 end
 
